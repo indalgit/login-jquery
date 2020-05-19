cf09b1e6e6ef989ab92940fded139ad0e9e32fb8
prince kumar <indalkumar19@gmail.com>
2020-05-19 10:59:39 +0530

    new

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -6,7 +6,6 @@
 
 
 
-
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery'), require('popper.js')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery', 'popper.js'], factory) :
